c8fe33da6c57ffb1a9e3742f3b82e7655d44d59a
Karel Dohnal <karel.dohnal@gmail.com>
2022-04-13 18:12:33 +0200

    příklad theme

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,9 +3,11 @@ import LanguageSwitch from '../utils/languages/LanguageSwitch';
 import ThemeSwitch from '../utils/theme/ThemeSwitch';
 import { useLanguage } from '../context/LanguageContext';
 import Header from '../components/Header';
+import {useTheme} from "../context/ThemeContext";
 
 function LandingPage() {
   const { dictionary } = useLanguage();
+  const { theme } = useTheme()
 
   return (
     <div>
@@ -13,6 +15,9 @@ function LandingPage() {
       <LanguageSwitch />
       <ThemeSwitch />
       {dictionary.navbar.about}
+      <p>Tady je stringified aktivní theme:</p>
+      <p>{JSON.stringify(theme)}</p>
+      <div style={{backgroundColor: theme.colors.menuBtn1}}>Nastylovaný div</div>
     </div>
   );
 }
